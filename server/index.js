1d2c1734d8eb09a286513b75e2148dc01206998a
Bhanuprasad321 <kanugulabhanu012@gmail.com>
2025-07-01 21:39:58 +0530

    Fix: Bind Express server to 0.0.0.0 and use process.env.PORT

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const QRcode = require("qrcode");
 const app = express();
 app.use(cors());
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.post("/shorten", async (req, res) => {
@@ -35,6 +35,9 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`running at URL - http://localhost:${port}`);
+
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
